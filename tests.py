c282e2496d6a9938685cc49c90d89b1eacf89244
FoyerD <amitfoye@post.bgu.ac.il>
2025-03-31 19:29:03 +0300

    finished 2.1

--- tests.py
+++ tests.py
@@ -1,4 +1,3 @@
-set1 = set([1, 2, 3, 4, 5])
-set2 = set([4, 5, 6, 7, 8])
-print(set1-set2)
-print(set1-set2)
+from itertools import product
+sett = set(map(lambda x: "".join(x), set(product('1234', repeat=2))))
+print(*sett)
